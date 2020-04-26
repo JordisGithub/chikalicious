7dd44254eb58910bd8bba67febe8d5adf236d78c
Janice He <aikeiti@Dominics-MBP.home>
2020-04-26 13:23:36 -0400

    favicon edit

---
+++ chika/public/Hi-Fi Desktop - Chikalicous - Zeplin_files/hodor.0f25f86efd.js
@@ -0,0 +1,2 @@
+webpackJsonp([27],{1714:function(module,exports,__webpack_require__){"use strict";function supportsAsyncAwait(){try{return eval("async () => {}"),!0}catch(t){return!1}}supportsAsyncAwait()||location.replace("/hodor")}},[1714]);
+//# sourceMappingURL=maps/hodor.map.js
