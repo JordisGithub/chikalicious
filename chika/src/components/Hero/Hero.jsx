5c24e5b908fd0d277c2331efcaec2cf933653339
Jordania Pereyra <Jordania.Pereyra@gmail.com>
2020-04-27 15:38:58 -0400

    hero

--- chika/src/components/Hero/Hero.jsx
+++ chika/src/components/Hero/Hero.jsx
@@ -1,22 +1,19 @@
 import React from "react";
-import HeroImg from "../../images/ChikaliciousHeaderImage.png"
-import "./Hero.css"
+import HeroImg from "../../images/ChikaliciousHeaderImage.png";
+import "./Hero.css";
 
-function Hero (props) {
+function Hero(props) {
   return (
-    <>
     <div className="hero-container">
-    <div className="hero-image">
-      <img src={HeroImg} alt="hero"></img>
+      <img className="hero-image" src={HeroImg} alt="hero"></img>
+
+      <div className="hero-text-block">
+        <h2 className="heroTitle">We Dessert First.</h2>
+        <h4 className="heroSlogan">Even at home.</h4>
+        <button className="hero-button">Order Online</button>
+      </div>
     </div>
-    <div>
-    <h2 className="heroTitle">We Dessert First.</h2>
-    <h4 className="heroSlogan">Even at home.</h4>
-    <button>Order Online</button>
-    </div>
-    </div>
-    </>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
