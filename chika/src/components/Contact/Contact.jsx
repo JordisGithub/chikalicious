26ba79882a6c0a00bd0c55c3bf9206915284ae95
Janice He <aikeiti@Dominics-MBP.home>
2020-04-27 23:34:25 -0400

    contact

--- chika/src/components/Contact/Contact.jsx
+++ chika/src/components/Contact/Contact.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import Location from "../Location";
-import Social from "../Social";
-import "./Contact.css"
+import Location from "./Location.jsx";
+import Social from "./Social.jsx";
+import "./Contact.css";
+import { Component } from "react";
 
-function Contact () {
-  return (
-    <>
-
-    </>
-  )
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="contact-container">
+        <Location />
+        <Social />
+      </div>
+    );
+  }
 }
 
-export default Contact
+export default Contact;
