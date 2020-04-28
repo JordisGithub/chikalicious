7199de07d9a49b0c6ff171787b96a455a69eb8fb
Janice He <aikeiti@Dominics-MBP.home>
2020-04-27 21:55:14 -0400

    location and review border fixed

---
+++ chika/src/components/Contact/Location.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Location() {
+  return (
+    <>
+      <div className="location-container">
+        <h2></h2>
+        <div>
+          <div className="map-img">
+            <img src="" alt="" />
+          </div>
+          <div className="transportation">
+            <section></section>
+            <section></section>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Location;
