dc4fb6090eddf677414ed567a3070b8e7d173f8b
Gabrielle <eastongabrielle@gmail.com>
2020-04-27 21:54:55 -0400

    hamburger

--- chika/src/App.js
+++ chika/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { render } from 'react-dom';
 import Nav from "./components/Nav/Nav";
@@ -6,6 +6,7 @@ import MobileNav from './components/Nav/MobileNav';
 import Hero from './components/Hero/Hero';
 import Reviews from "./components/Reviews/Reviews";
 import Footer from './components/Footer/Footer';
+import BackDrop from './components/Nav/BackDrop'
 // import Hero from "./components/Hero/Hero";
 import HeroFoodPhotos from "./components/HeroFoodPhotos/HeroFoodPhotos";
 // import Menu from "./components/Menu/Menu";
@@ -15,18 +16,38 @@ import OurStory from "./components/OurStory/OurStory";
 // import PhotoGallery from "./components/PhotoGallery/PhotoGallery";
 // import Footer from "./components/Footer/Footer";
 
-function App() {
-  return (
-    <>
-      <Nav />
-      <MobileNav />
-      <Hero />
-      <HeroFoodPhotos />
-      <OurStory />
-      <Reviews />
-      <Footer />
-    </>
-  );
-}
+class App extends Component {
+  state = {
+    mobileMenuOpen: false
+  }
+  drawerToggleClickHandler = () => {
+    this.setState((prevState) => {
+      return { mobileMenuOpen: !prevState.mobileMenuOpen }
+    })
+  };
+  backdropClickHandler = () => {
+    this.setState({ mobileMenuOpen: false })
+  }
 
+  render() {
+    let backDrop;
+    if (this.state.mobileMenuOpen) {
+      backDrop = <BackDrop click={this.backdropClickHandler} />;
+    }
+
+
+    return (
+      <>
+        <Nav drawerClickHandler={this.drawerToggleClickHandler} />
+        <MobileNav show={this.state.mobileMenuOpen} />
+        {backDrop}
+        <Hero />
+        <HeroFoodPhotos />
+        <OurStory />
+        <Reviews />
+        <Footer />
+      </>
+    );
+  }
+}
 export default App;
--- chika/src/components/Nav/MobileNav.jsx
+++ chika/src/components/Nav/MobileNav.jsx
@@ -3,21 +3,26 @@ import logo from "../../images/logo.png";
 import "./MobileNav.css";
 
 const MobileNav = props => {
+  let mobileClasses = "mobile-container";
+  if (props.show) {
+    mobileClasses = "mobile-container open"
+  }
+
   return (
-    <div className="mobile-container">
-    <div className="mobileLogo">
-    <img src={logo} alt="Chikalicious"></img>
-    </div>
-    <div className="nav-M">
-    <ul>
-      <li className="linksM"><a href="#">Menu</a></li>
-      <li className="linksM"><a href="#">Our Story</a></li>
-      <li className="linksM"><a href="#">Support Us</a></li>
-      <li className="linksM"><a href="#">Location & Hours</a></li>
-      <li className="linksM"><a href="#">Contact Us</a></li>
-      <li className="linksM"><a href="#">Photo Gallery</a></li>
-    </ul>
-    </div>
+    <div className={mobileClasses}>
+      {/* <div className="mobileLogo">
+        <img src={logo} alt="Chikalicious"></img>
+      </div> */}
+      <div className="nav-M">
+        <ul>
+          <li className="linksM"><a href="#">Menu</a></li>
+          <li className="linksM"><a href="#">Our Story</a></li>
+          <li className="linksM"><a href="#">Support Us</a></li>
+          <li className="linksM"><a href="#">Location & Hours</a></li>
+          <li className="linksM"><a href="#">Contact Us</a></li>
+          <li className="linksM"><a href="#">Photo Gallery</a></li>
+        </ul>
+      </div>
     </div>
   )
 }
