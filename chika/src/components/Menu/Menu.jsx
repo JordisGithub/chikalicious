b71b2d53349cd2b2de76b09e3039fc360d5f7b7b
Janice He <aikeiti@Dominics-MBP.home>
2020-04-28 21:51:50 -0400

    menu started

---
+++ chika/src/components/Menu/Menu.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react"; 
+import Drink from "../../images/drink.png";
+import Food from "../../images/food.png";
+import "./Menu.css"
+
+export default class Menu extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      menuPhoto: {Food}
+    }
+  }
+
+  render() {
+    return (
+      <>
+      </>
+    )
+  }
+}
