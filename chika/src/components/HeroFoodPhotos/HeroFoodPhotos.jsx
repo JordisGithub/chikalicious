980971162fda7bd5c2f5ecb33b740802b6c381a0
Jordania Pereyra <Jordania.Pereyra@gmail.com>
2020-04-29 23:01:12 -0400

    css updates on hero, grid and mobile hero

--- chika/src/components/HeroFoodPhotos/HeroFoodPhotos.jsx
+++ chika/src/components/HeroFoodPhotos/HeroFoodPhotos.jsx
@@ -1,40 +1,31 @@
 import React from "react";
 import "./HeroFoodPhotos.css";
 
-import Prixfix from "../../images/Prix_fixe.png";
-import PrixfixCofee from "../../images/Prix_fix_with_coffee.png";
-import PrixfixTea from "../../images/Prix_fixe_with_tea.JPG";
-import PrixfixWine from "../../images/Prix_fixe_with_Wine.png";
-
 function HeroFoodPhotos(props) {
   return (
     <>
       <div className="hero-photo-grid-container">
         <div>
-          <div className="hero-photo-div">
-            {/* <img className="hero-grid-ind-img" src={Prixfix} /> */}
-          </div>
+          <div className="hero-photo-div"></div>
+          {/* <img className="hero-grid-ind-img" src={Prixfix} /> */}
           <span className="photo-Title">Prix Fix</span>
         </div>
 
         <div>
-          <div className="hero-photo-div">
-            {/* <img className="hero-grid-ind-img" src={PrixfixCofee} /> */}
-          </div>
+          <div className="hero-photo-div2"></div>
+          {/* <img className="hero-grid-ind-img" src={PrixfixCofee} /> */}
           <span className="photo-Title">Prix Fix Coffee</span>
         </div>
 
         <div>
-          <div className="hero-photo-div">
-            {/* <img className="hero-grid-ind-img" src={PrixfixTea} /> */}
-          </div>
+          <div className="hero-photo-div3"></div>
+          {/* <img className="hero-grid-ind-img" src={PrixfixTea} /> */}
           <span className="photo-Title">Prix Fix Tea</span>
         </div>
 
         <div>
-          <div className="hero-photo-div">
-            {/* <img className="hero-grid-ind-img" src={PrixfixWine} /> */}
-          </div>
+          <div className="hero-photo-div4"></div>
+          {/* <img className="hero-grid-ind-img" src={PrixfixWine} /> */}
           <span className="photo-Title">Prix Fix Wine</span>
         </div>
       </div>
