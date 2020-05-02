83cdd0caeed3df009315a3212f7d5d3323daf6e3
Janice He <aikeiti@Dominics-MBP.home>
2020-05-02 13:18:46 -0400

    reviews

--- chika/src/components/Reviews/Reviews.jsx
+++ chika/src/components/Reviews/Reviews.jsx
@@ -7,45 +7,58 @@ import "./Reviews.css";
 
 function Reviews(props) {
   return (
-    <div className="review-containers">
-      <h2 className="reviewsTitle">Reviews</h2>
-      <div className="reviews">
-        <section>
-          <img src={Ashley} alt="Ashley" />
-          <p className="web">
-            See more on{" "}
-            <span>
-              <a
-                href="https://www.yelp.com/biz/chikalicious-dessert-bar-new-york-3"
-                target="_blank"
-              >
-                Yelp
-              </a>
-            </span>
-          </p>
-        </section>
+    <>
+    <div className="review-header">
+    <h2 className="reviewsTitle">Reviews</h2> 
+    </div>
 
-        <section>
-          <img src={Rpoonai} alt="Rpoonai" />
-          <p className="web">
+    <div className="review-containers">
+
+      <div className="reviews">
+      {/* <img src="" alt=""/> */}
+
+      <p>This will be the best meal you'll ever
+        experience. And yes, dessert. You will leave this place
+        changed for the better. Chika we love you!</p>
+      {/* <img src="" alt=""/> */}
+
+      <p>-Ashley A.</p>
+
+      <p>
+          See more on{" "}
+        <span>
+        <a
+          href="https://www.yelp.com/biz/chikalicious-dessert-bar-new-york-3"
+          target="_blank">Yelp</a>
+        </span>
+      </p>
+      </div>
+
+      <div className="reviews">
+      {/* <img src="" alt=""/> */}
+      <p>This is such a wonderful place for a date night,
+        girls' night out or any special celebration. The desserts
+        were artfully presented. A definited must try.
+      </p>
+      {/* <img src="" alt=""/> */}
+      <p>-Rpoonai</p>
+      <p className="web">
             See more on{" "}
             <span>
-              <a
-                href="https://www.google.com/search?sxsrf=ALeKk01IMewqBIBnO5DNDWuzBbQkedcIHg%3A1588024942502&ei=blanXqOUHqiwytMPjpScgAo&q=chikalicious+google+reviews&oq=chikalicious+google+re&gs_lcp=CgZwc3ktYWIQARgAMgUIIRCgATIFCCEQoAEyBQghEKsCMgUIIRCrAjoFCAAQzQJQwxxYsR5g6SRoAHAAeACAAZ0BiAHrBJIBAzAuNZgBAKABAaoBB2d3cy13aXo&sclient=psy-ab#lrd=0x89c2599c49f749b7:0x714d802b46ffd353,1,,,"
-                target="_blank"
-              >
-                Google
-              </a>
+              <a href="https://www.google.com/search?sxsrf=ALeKk01IMewqBIBnO5DNDWuzBbQkedcIHg%3A1588024942502&ei=blanXqOUHqiwytMPjpScgAo&q=chikalicious+google+reviews&oq=chikalicious+google+re&gs_lcp=CgZwc3ktYWIQARgAMgUIIRCgATIFCCEQoAEyBQghEKsCMgUIIRCrAjoFCAAQzQJQwxxYsR5g6SRoAHAAeACAAZ0BiAHrBJIBAzAuNZgBAKABAaoBB2d3cy13aXo&sclient=psy-ab#lrd=0x89c2599c49f749b7:0x714d802b46ffd353,1,,,"
+                target="_blank">Google</a>
             </span>
-          </p>
-        </section>
-      </div>
-        <div className="scrollTop">
-        <ScrollUpButton style={{position: "flex", height: "15px", width: "15px", backgroundColor: "transparent", color: "#f3c26c"}}/>
-        <p>Back to Top</p>
+      </p>
+    </div>
+
+      <div className="scrollTop">
+          <ScrollUpButton style={{position: "flex", height: "15px", width: "15px", backgroundColor: "transparent", color: "#f3c26c"}}/>
+      <p>Back to Top</p>
       </div>
     </div>
-  );
-}
-
+    </>
+    
+    );
+  }
+      
 export default Reviews;
