7c25f71a50e0d10fcf09ca32cf13eb13bac03cac
Janice He <aikeiti@Dominics-MBP.home>
2020-05-02 21:49:59 -0400

    support us fixed

--- chika/src/components/SupportUs/SupportUs.jsx
+++ chika/src/components/SupportUs/SupportUs.jsx
@@ -13,7 +13,7 @@ const SupportUs = () => (
 
     <div className="container-support">
 
-      <div>
+      <div className="support-description-desktop">
         <div className="support-description">
           We ask for your help in this time of uncertainty. Please support our
           staff directly by donating to our GoFundMe link: <br />
@@ -21,28 +21,27 @@ const SupportUs = () => (
           Thank you!
         </div>
 
-        <a href="#">Staff GoFundMe </a>
+        <span style={{fontSize: "24px", fontWeight: "300"}}><a href="#"className="support-link">Staff GoFundMe</a><span style={{textDecoration: "none", color: "#ffe5b9", fontSize: "24px"}}>></span></span>
       </div>
 
       <div className="support-description-desktop">
-        <div>
+        <div className="support-descriptionn">
           You can also purchase gift certificates, which help us support our
           staff and suppliers. <br />
           <br />
           Thank you!
         </div>
 
-        <a
+        <span style={{fontSize: "24px", fontWeight: "300"}}><a className="support-link"
           href="https://onelink.quickgifts.com/merchant/chikalicious/"
           target="_blank"
         >
-          Gift Certificates
-        </a>
+          Gift Certificates</a><span style={{textDecoration: "none", color: "#ffe5b9", fontSize: "24px"}}>></span></span>
 
       </div>
 
       <div className="support-pic">
-        <img src={staff} alt="lady cooking dessert" />
+        <img className="cook-staff" src={staff} alt="lady cooking dessert" />
       </div>
 
       <div className="support-description-mobile">
